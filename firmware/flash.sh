0c4f7a1b004b38663f11aa12e69f66088d423f4f
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2025-02-13 20:46:24 +0100

    misc: add simple fdd flasher script

---
+++ firmware/flash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# arg - path to FDD mountpoint
+set -e
+
+./build.sh all
+cp kernel/kernel.bin $1/BOOT/KERNEL.IMG
+cp usr/hello/hello.bin $1/BIN/HELLO.ZEX
+sync
+sudo umount $1
+sync
+
+echo "done"
